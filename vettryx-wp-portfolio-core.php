90aa70932e6e007335bef136e9dbe51118991419
André Ventura <ti@asventura.com.br>
2026-02-24 16:04:33 -0300

    Adiciona o Plugin Update Checker ao projeto para habilitar atualizações automáticas no WordPress do Projeto

--- vettryx-wp-portfolio-core.php
+++ vettryx-wp-portfolio-core.php
@@ -2,16 +2,29 @@
 /**
  * Plugin Name: VETTRYX WP Portfolio Core
  * Description: Plugin Core para o site da VETTRYX. Gerencia Portfólio, Skills e Shortcodes Institucionais.
- * Version:     1.1.0
+ * Version:     1.0.0
  * Author:      VETTRYX Tech
  * Author URI:  https://vettryx.com.br
  * License:     GPLv3 or later
  */
 
-// Bloqueia acesso direto ao arquivo por segurança
-if (!defined('ABSPATH')) {
-    exit;
+if (!defined('ABSPATH')) exit;
+
+// --- INÍCIO DA ATUALIZAÇÃO AUTOMÁTICA (GITHUB) ---
+// Define o caminho exato do arquivo da biblioteca
+$puc_file = plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker.php';
+
+// Só executa a atualização se a pasta da biblioteca realmente existir
+if (file_exists($puc_file)) {
+    require $puc_file;
+    
+    $myUpdateChecker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
+        'https://github.com/vettryx/vettryx-wp-portfolio-core',
+        __FILE__,
+        'vettryx-wp-portfolio-core'
+    );
 }
+// --- FIM DA ATUALIZAÇÃO AUTOMÁTICA ---
 
 // Carrega os módulos da pasta includes
 require_once plugin_dir_path(__FILE__) . 'includes/post-types.php';
